d04d607be5b1f40ba6b19fc4ab4257100a9247db
Alex Efremov <151593933+A1exE0v@users.noreply.github.com>
2023-11-29 22:03:14 -0800

    Update main script

--- weebro.py
+++ weebro.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python3
 #
-# natbot.py
+# weebro.py
 #
 # Set OPENAI_API_KEY to your API key, and then run this from a terminal.
 #
 
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError
 import time
+import sys
 from sys import argv, exit, platform
-import openai
+from openai import OpenAI  # Updated import
+from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth_sync
 import os
 
 quiet = False
@@ -21,130 +24,6 @@ if len(argv) >= 2:
 		)
 
 prompt_template = """
-You are an agent controlling a browser. You are given:
-
-	(1) an objective that you are trying to achieve
-	(2) the URL of your current web page
-	(3) a simplified text description of what's visible in the browser window (more on that below)
-
-You can issue these commands:
-	SCROLL UP - scroll up one page
-	SCROLL DOWN - scroll down one page
-	CLICK X - click on a given element. You can only click on links, buttons, and inputs!
-	TYPE X "TEXT" - type the specified text into the input with id X
-	TYPESUBMIT X "TEXT" - same as TYPE above, except then it presses ENTER to submit the form
-
-The format of the browser content is highly simplified; all formatting elements are stripped.
-Interactive elements such as links, inputs, buttons are represented like this:
-
-		<link id=1>text</link>
-		<button id=2>text</button>
-		<input id=3>text</input>
-
-Images are rendered as their alt text like this:
-
-		<img id=4 alt=""/>
-
-Based on your given objective, issue whatever command you believe will get you closest to achieving your goal.
-You always start on Google; you should submit a search query to Google that will take you to the best page for
-achieving your objective. And then interact with that page to achieve your objective.
-
-If you find yourself on Google and there are no search results displayed yet, you should probably issue a command 
-like "TYPESUBMIT 7 "search query"" to get to a more useful page.
-
-Then, if you find yourself on a Google search results page, you might issue the command "CLICK 24" to click
-on the first link in the search results. (If your previous command was a TYPESUBMIT your next command should
-probably be a CLICK.)
-
-Don't try to interact with elements that you can't see.
-
-Here are some examples:
-
-EXAMPLE 1:
-==================================================
-CURRENT BROWSER CONTENT:
-------------------
-<link id=1>About</link>
-<link id=2>Store</link>
-<link id=3>Gmail</link>
-<link id=4>Images</link>
-<link id=5>(Google apps)</link>
-<link id=6>Sign in</link>
-<img id=7 alt="(Google)"/>
-<input id=8 alt="Search"></input>
-<button id=9>(Search by voice)</button>
-<button id=10>(Google Search)</button>
-<button id=11>(I'm Feeling Lucky)</button>
-<link id=12>Advertising</link>
-<link id=13>Business</link>
-<link id=14>How Search works</link>
-<link id=15>Carbon neutral since 2007</link>
-<link id=16>Privacy</link>
-<link id=17>Terms</link>
-<text id=18>Settings</text>
-------------------
-OBJECTIVE: Find a 2 bedroom house for sale in Anchorage AK for under $750k
-CURRENT URL: https://www.google.com/
-YOUR COMMAND: 
-TYPESUBMIT 8 "anchorage redfin"
-==================================================
-
-EXAMPLE 2:
-==================================================
-CURRENT BROWSER CONTENT:
-------------------
-<link id=1>About</link>
-<link id=2>Store</link>
-<link id=3>Gmail</link>
-<link id=4>Images</link>
-<link id=5>(Google apps)</link>
-<link id=6>Sign in</link>
-<img id=7 alt="(Google)"/>
-<input id=8 alt="Search"></input>
-<button id=9>(Search by voice)</button>
-<button id=10>(Google Search)</button>
-<button id=11>(I'm Feeling Lucky)</button>
-<link id=12>Advertising</link>
-<link id=13>Business</link>
-<link id=14>How Search works</link>
-<link id=15>Carbon neutral since 2007</link>
-<link id=16>Privacy</link>
-<link id=17>Terms</link>
-<text id=18>Settings</text>
-------------------
-OBJECTIVE: Make a reservation for 4 at Dorsia at 8pm
-CURRENT URL: https://www.google.com/
-YOUR COMMAND: 
-TYPESUBMIT 8 "dorsia nyc opentable"
-==================================================
-
-EXAMPLE 3:
-==================================================
-CURRENT BROWSER CONTENT:
-------------------
-<button id=1>For Businesses</button>
-<button id=2>Mobile</button>
-<button id=3>Help</button>
-<button id=4 alt="Language Picker">EN</button>
-<link id=5>OpenTable logo</link>
-<button id=6 alt ="search">Search</button>
-<text id=7>Find your table for any occasion</text>
-<button id=8>(Date selector)</button>
-<text id=9>Sep 28, 2022</text>
-<text id=10>7:00 PM</text>
-<text id=11>2 people</text>
-<input id=12 alt="Location, Restaurant, or Cuisine"></input> 
-<button id=13>Let’s go</button>
-<text id=14>It looks like you're in Peninsula. Not correct?</text> 
-<button id=15>Get current location</button>
-<button id=16>Next</button>
-------------------
-OBJECTIVE: Make a reservation for 4 for dinner at Dorsia in New York City at 8pm
-CURRENT URL: https://www.opentable.com/
-YOUR COMMAND: 
-TYPESUBMIT 12 "dorsia new york city"
-==================================================
-
 The current browser content, objective, and current URL follow. Reply with your next command to the browser.
 
 CURRENT BROWSER CONTENT:
@@ -162,19 +41,37 @@ black_listed_elements = set(["html", "head", "title", "meta", "iframe", "body", 
 
 class Crawler:
 	def __init__(self):
-		self.browser = (
-			sync_playwright()
-			.start()
-			.chromium.launch(
-				headless=False,
-			)
-		)
+		# Start Playwright in a synchronous context
+		self.playwright = sync_playwright().start()
+		
+		# Launch the browser
+		self.browser = self.playwright.chromium.launch(headless=False,
+			args=[
+			"--disable-notifications",
+			#"--disable-gpu",
+			#"--disable-setuid-sandbox",
+			#"--deterministic-fetch",
+			#"--disable-features=IsolateOrigins,site-per-process",
+			#"--disable-site-isolation-trials",
+			#"--disable-web-security",
+			#"--disable-blink-features=AutomationControlled",
+			#"--disable-dev-shm-usage"
+			]
+   		)
 
+		# Create a new page
 		self.page = self.browser.new_page()
-		self.page.set_viewport_size({"width": 1280, "height": 1080})
+
+		# Apply stealth
+		# stealth_sync(self.page)
+
+		# Set viewport size
+		# self.page.set_viewport_size({"width": 1280, "height": 1080})
+
 
 	def go_to_page(self, url):
 		self.page.goto(url=url if "://" in url else "http://" + url)
+		self.page.wait_for_load_state()
 		self.client = self.page.context.new_cdp_session(self.page)
 		self.page_element_buffer = {}
 
@@ -215,13 +112,33 @@ class Crawler:
 		self.page.keyboard.press("Enter")
 
 	def crawl(self):
+		start = time.time()
 		page = self.page
 		page_element_buffer = self.page_element_buffer
 		start = time.time()
 
 		page_state_as_text = []
+  
+		page.wait_for_load_state()
+  
+		retry_limit = 3
+  
+		for attempt in range(retry_limit):
+			try:
+				device_pixel_ratio = self.page.evaluate("window.devicePixelRatio")
+				page_state_as_text.append(f"Device Pixel Ratio: {device_pixel_ratio}")
+				break  # Break the loop if successful
+			except TimeoutError as e:
+				if attempt < retry_limit - 1:
+					print(f"Retrying due to error: {e}")
+					time.sleep(1)  # Delay before retrying
+				else:
+					print("Maximum retries reached, aborting.")
+					raise  # Re-raise the exception on the last attempt
 
-		device_pixel_ratio = page.evaluate("window.devicePixelRatio")
+
+			#	device_pixel_ratio = page.evaluate("window.devicePixelRatio")
+    
 		if platform == "darwin" and device_pixel_ratio == 1:  # lies
 			device_pixel_ratio = 2
 
@@ -293,6 +210,10 @@ class Crawler:
 		def convert_name(node_name, has_click_handler):
 			if node_name == "a":
 				return "link"
+			if node_name == "textarea":
+				return "input"
+			if node_name == "label":
+				return "text"
 			if node_name == "input":
 				return "input"
 			if node_name == "img":
@@ -398,7 +319,7 @@ class Crawler:
 
 			# inefficient to grab the same set of keys for kinds of objects but its fine for now
 			element_attributes = find_attributes(
-				attributes[index], ["type", "placeholder", "aria-label", "title", "alt"]
+				attributes[index], ["type", "placeholder", "aria-label", "title", "alt", "role"]
 			)
 
 			ancestor_exception = is_ancestor_of_anchor or is_ancestor_of_button
@@ -528,22 +449,23 @@ class Crawler:
 
 			if inner_text != "": 
 				elements_of_interest.append(
-					f"""<{converted_node_name} id={id_counter}{meta}>{inner_text}</{converted_node_name}>"""
+					f"""<{converted_node_name} {id_counter}{meta}>{inner_text}</{converted_node_name}>"""
 				)
 			else:
 				elements_of_interest.append(
-					f"""<{converted_node_name} id={id_counter}{meta}/>"""
+					f"""<{converted_node_name} {id_counter}{meta}/>"""
 				)
 			id_counter += 1
 
 		print("Parsing time: {:0.2f} seconds".format(time.time() - start))
 		return elements_of_interest
 
-if (
-	__name__ == "__main__"
-):
+
+if (__name__ == "__main__"):
 	_crawler = Crawler()
-	openai.api_key = os.environ.get("OPENAI_API_KEY")
+	client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))  # Updated OpenAI client initialization
+	assistant_id = "asst_3ATpt3bETgsd024ascqsi87p"
+	thread = client.beta.threads.create()
 
 	def print_help():
 		print(
@@ -557,8 +479,57 @@ if (
 		prompt = prompt.replace("$url", url[:100])
 		prompt = prompt.replace("$previous_command", previous_command)
 		prompt = prompt.replace("$browser_content", browser_content[:4500])
-		response = openai.Completion.create(model="text-davinci-002", prompt=prompt, temperature=0.5, best_of=10, n=3, max_tokens=50)
-		return response.choices[0].text
+  
+		# return "AI TURNED OFF"
+		
+		# response = client.completions.create(model="text-davinci-002", prompt=prompt, temperature=0.5, best_of=10, n=3, max_tokens=50)  # Updated method call
+
+        # Add a message to the existing thread
+		client.beta.threads.messages.create(
+            thread_id=thread.id,
+            role="user",
+            content=prompt
+        )
+
+        # Run the assistant using the existing assistant ID
+		run = client.beta.threads.runs.create(
+            thread_id=thread.id,
+            assistant_id=assistant_id
+        )
+		
+		while True:
+			run_status = client.beta.threads.runs.retrieve(
+				thread_id=thread.id,
+				run_id=run.id
+			)
+            
+			# Check if the run has completed or encountered an error
+			if run_status.status in ["completed"]:
+				break
+			elif run_status.status in ["cancelled", "failed", "expired", "requires_action"]:
+				error_message = "Run encountered a problem: " + run_status.status
+				if run_status.last_error is not None:
+					error_message += f"\nError Code: {run_status.last_error.code}"
+					error_message += f"\nError Message: {run_status.last_error.message}"
+				print()
+				print(error_message, file=sys.stderr)
+				exit(0)
+			elif run_status.status in ["queued", "in_progress", "cancelling"]:
+				time.sleep(0.2)  # Wait for a 0.2 second before checking again
+
+		# Retrieve messages from the run
+		messages = client.beta.threads.messages.list(
+			thread_id=thread.id
+		)
+
+		# Extract the assistant's response
+		for message in messages.data:
+			if message.role == "assistant":
+				return message.content[0].text.value.strip()
+
+		return "No response received"
+
+		## return response.choices[0].text
 
 	def run_cmd(cmd):
 		cmd = cmd.split("\n")[0]
@@ -582,11 +553,14 @@ if (
 			if cmd.startswith("TYPESUBMIT"):
 				text += '\n'
 			_crawler.type(id, text)
+		elif cmd.startswith("GOTO"):
+			spacesplit = cmd.split(" ")
+			_crawler.go_to_page(spacesplit[1])
 
 		time.sleep(2)
 
 	objective = "Make a reservation for 2 at 7pm at bistro vida in menlo park"
-	print("\nWelcome to natbot! What is your objective?")
+	print("\nWelcome to Weebro.ai! What is your objective?")
 	i = input()
 	if len(i) > 0:
 		objective = i
@@ -599,7 +573,7 @@ if (
 			browser_content = "\n".join(_crawler.crawl())
 			prev_cmd = gpt_cmd
 			gpt_cmd = get_gpt_command(objective, _crawler.page.url, prev_cmd, browser_content)
-			gpt_cmd = gpt_cmd.strip()
+			# gpt_cmd = gpt_cmd[0].text.value.strip()
 
 			if not quiet:
 				print("URL: " + _crawler.page.url)
@@ -608,32 +582,36 @@ if (
 			if len(gpt_cmd) > 0:
 				print("Suggested command: " + gpt_cmd)
 
-
-			command = input()
-			if command == "r" or command == "":
-				run_cmd(gpt_cmd)
-			elif command == "g":
-				url = input("URL:")
-				_crawler.go_to_page(url)
-			elif command == "u":
-				_crawler.scroll("up")
-				time.sleep(1)
-			elif command == "d":
-				_crawler.scroll("down")
-				time.sleep(1)
-			elif command == "c":
-				id = input("id:")
-				_crawler.click(id)
-				time.sleep(1)
-			elif command == "t":
-				id = input("id:")
-				text = input("text:")
-				_crawler.type(id, text)
-				time.sleep(1)
-			elif command == "o":
-				objective = input("Objective:")
+			if gpt_cmd.startswith("GOAL ACHIEVED") or gpt_cmd.startswith("OUTPUT"):
+   
+				command = input()
+				if command == "r" or command == "":
+					run_cmd(gpt_cmd)
+				elif command == "g":
+					url = input("URL:")
+					_crawler.go_to_page(url)
+				elif command == "u":
+					_crawler.scroll("up")
+					time.sleep(1)
+				elif command == "d":
+					_crawler.scroll("down")
+					time.sleep(1)
+				elif command == "c":
+					id = input("id:")
+					_crawler.click(id)
+					time.sleep(1)
+				elif command == "t":
+					id = input("id:")
+					text = input("text:")
+					_crawler.type(id, text)
+					time.sleep(1)
+				elif command == "o":
+					objective = input("Objective:")
+				else:
+					print_help()
 			else:
-				print_help()
+					run_cmd(gpt_cmd)
+
 	except KeyboardInterrupt:
 		print("\n[!] Ctrl+C detected, exiting gracefully.")
 		exit(0)
